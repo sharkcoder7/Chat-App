f450ebcfb5ab93b9ee5598762670d7e198f2e4ed
denebtech <techdeneb@gmail.com>
2019-03-06 22:36:25 -0500

    getlocation support

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -23,6 +24,10 @@ io.on('connection', (socket) => {
     console.log('got message', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from the server.');
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('User', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 
 describe('generateMessage', () => {
@@ -11,5 +11,20 @@ describe('generateMessage', () => {
     expect(res.from).toBe('jason');
     expect(res.text).toBe('Some message');
     expect(res.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'jon';
+    var lat = '26.3380777';
+    var long = '-80.1078862';
+    var url = 'https://www.google.com/maps/?q=26.3380777,-80.1078862';
+    var res = generateLocationMessage(from, lat, long);
+    console.log(res);
+    expect(res.from).toBe('jon');
+    expect(res.createdAt).toBeA('number');
+    expect(res.url).toBe(url);
+    expect(res).toInclude({from, url});
   });
 });
